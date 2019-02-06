c265fec3e9257ae49af5f998f5deae8226cb3c34
gmDefault <cedric.lafrasse@gmail.com>
2019-02-06 16:03:29 +0100

    Pushing angular material integration generator for Jhipster

---
+++ README.md
@@ -0,0 +1,57 @@
+# generator-jhipster-angular-material-integration
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> JHipster module, Module for installing the dependencies needed to use Angular Material in a Jhipster project.
+
+# Introduction
+
+This is a [JHipster](http://jhipster.github.io/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](http://jhipster.github.io/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://jhipster.github.io/installation.html)
+
+# Installation
+
+## With Yarn
+
+To install this module:
+
+```bash
+yarn global add generator-jhipster-angular-material-integration
+```
+
+To update this module:
+
+```bash
+yarn global upgrade generator-jhipster-angular-material-integration
+```
+
+## With NPM
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-angular-material-integration
+```
+
+To update this module:
+
+```bash
+npm update -g generator-jhipster-angular-material-integration
+```
+
+# Usage
+
+# License
+
+MIT © [Contribution Jhipster UGA](https://github.com/contribution-jhipster-uga)
+
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-angular-material-integration.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-angular-material-integration
+[travis-image]: https://travis-ci.org/contribution-jhipster-uga/generator-jhipster-angular-material-integration.svg?branch=master
+[travis-url]: https://travis-ci.org/contribution-jhipster-uga/generator-jhipster-angular-material-integration
+[daviddm-image]: https://david-dm.org/contribution-jhipster-uga/generator-jhipster-angular-material-integration.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/contribution-jhipster-uga/generator-jhipster-angular-material-integration
---
+++ generators/app/index.js
@@ -0,0 +1,162 @@
+const chalk = require('chalk');
+const packagejs = require('../../package.json');
+const semver = require('semver');
+const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const jhipsterConstants = require('generator-jhipster/generators/generator-constants');
+const jhipsterUtils = require('generator-jhipster/generators/utils');
+
+module.exports = class extends BaseGenerator {
+    get initializing() {
+        return {
+            init(args) {
+                if (args === 'default') {
+                    // do something when argument is 'default'
+                }
+            },
+            readConfig() {
+                this.jhipsterAppConfig = this.getAllJhipsterConfig();
+                if (!this.jhipsterAppConfig) {
+                    this.error('Can\'t read .yo-rc.json');
+                }
+            },
+            displayLogo() {
+                // it's here to show that you can use functions from generator-jhipster
+                // this function is in: generator-jhipster/generators/generator-base.js
+                this.printJHipsterLogo();
+
+                // Have Yeoman greet the user.
+                this.log(`\nWelcome to the ${chalk.bold.yellow('JHipster angular-material-integration')} generator! ${chalk.yellow(`v${packagejs.version}\n`)}`);
+            },
+            checkJhipster() {
+                const currentJhipsterVersion = this.jhipsterAppConfig.jhipsterVersion;
+                const minimumJhipsterVersion = packagejs.dependencies['generator-jhipster'];
+                if (!semver.satisfies(currentJhipsterVersion, minimumJhipsterVersion)) {
+                    this.warning(`\nYour generated project used an old JHipster version (${currentJhipsterVersion})... you need at least (${minimumJhipsterVersion})\n`);
+                }
+            }
+        };
+    }
+
+    writing() {
+        // function to use directly template
+        this.template = function(source, destination) {
+            this.fs.copyTpl(
+                this.templatePath(source),
+                this.destinationPath(destination),
+                this
+            );
+        };
+
+        // read config from .yo-rc.json
+        this.baseName = this.jhipsterAppConfig.baseName;
+        this.packageName = this.jhipsterAppConfig.packageName;
+        this.packageFolder = this.jhipsterAppConfig.packageFolder;
+        this.clientFramework = this.jhipsterAppConfig.clientFramework;
+        this.clientPackageManager = this.jhipsterAppConfig.clientPackageManager;
+        this.buildTool = this.jhipsterAppConfig.buildTool;
+
+        // use function in generator-base.js from generator-jhipster
+        this.angularAppName = this.getAngularAppName();
+
+        // use constants from generator-constants.js
+        const javaDir = `${jhipsterConstants.SERVER_MAIN_SRC_DIR + this.packageFolder}/`;
+        const resourceDir = jhipsterConstants.SERVER_MAIN_RES_DIR;
+        const webappDir = jhipsterConstants.CLIENT_MAIN_SRC_DIR;
+
+
+        // // show all variables
+        // this.log('\n--- some config read from config ---');
+        // this.log(`baseName=${this.baseName}`);
+        // this.log(`packageName=${this.packageName}`);
+        // this.log(`clientFramework=${this.clientFramework}`);
+        // this.log(`clientPackageManager=${this.clientPackageManager}`);
+        // this.log(`buildTool=${this.buildTool}`);
+        //
+        // this.log('\n--- some function ---');
+        // this.log(`angularAppName=${this.angularAppName}`);
+        //
+        // this.log('\n--- some const ---');
+        // this.log(`javaDir=${javaDir}`);
+        // this.log(`resourceDir=${resourceDir}`);
+        // this.log(`webappDir=${webappDir}`);
+
+        this.template('jh-material.module.ts', `${webappDir}app/shared/jh-material.module.ts`);
+        jhipsterUtils.rewriteFile({
+            file: `${webappDir}app/app.module.ts`,
+            needle: 'import * as moment from \'moment\';',
+            splicable: [`import { BrowserAnimationsModule } from '@angular/platform-browser/animations';`]
+        }, this);
+        jhipsterUtils.rewriteFile({
+            file: `${webappDir}app/app.module.ts`,
+            needle: 'jhipster-needle-angular-add-module-import',
+            splicable: [`import 'hammerjs';`]
+        }, this);
+        jhipsterUtils.rewriteFile({
+            file: `${webappDir}app/app.module.ts`,
+            needle: 'jhipster-needle-angular-add-module',
+            splicable: [`BrowserAnimationsModule,`]
+        }, this);
+        jhipsterUtils.rewriteFile({
+            file: `${webappDir}content/scss/vendor.scss`,
+            needle: 'jhipster-needle-scss-add-vendor',
+            splicable: [`@import "~@angular/material/prebuilt-themes/indigo-pink.css";`]
+        }, this);
+        jhipsterUtils.rewriteFile({
+          file: `${webappDir}app/shared/shared.module.ts`,
+          needle: '@NgModule({',
+          splicable: [`import {JhMaterialModule} from 'app/shared/jh-material.module';`]
+        }, this);
+        jhipsterUtils.rewriteFile({
+          file: `package.json`,
+          needle: '"dependencies": {',
+          splicable: [``]
+        }, this);
+
+        var sharedModule = this.fs.read(`${webappDir}app/shared/shared.module.ts`);
+        var res = sharedModule.replace('imports: [','imports: [JhMaterialModule, ');
+        var toWrite = res.replace('exports: [','exports: [JhMaterialModule, ');
+        this.fs.write(`${webappDir}app/shared/shared.module.ts`, toWrite);
+
+        var packageJson = this.fs.read(`package.json`);
+        var toWrite = packageJson.replace('"dependencies": {',`"dependencies": {
+      "hammerjs": "latest",
+      "@angular/material": "latest",
+      "@angular/cdk": "latest",
+      "@angular/animations": "latest",`);
+        this.fs.write(`package.json`, toWrite);
+    }
+
+
+    install() {
+        let logMsg =
+            `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install`)}`;
+
+        if (this.clientFramework === 'angular1') {
+            logMsg =
+                `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install & bower install`)}`;
+        }
+        const injectDependenciesAndConstants = (err) => {
+            if (err) {
+                this.warning('Install of dependencies failed!');
+                this.log(logMsg);
+            } else if (this.clientFramework === 'angular1') {
+                this.spawnCommand('gulp', ['install']);
+            }
+        };
+        const installConfig = {
+            bower: this.clientFramework === 'angular1',
+            npm: this.clientPackageManager !== 'yarn',
+            yarn: this.clientPackageManager === 'yarn',
+            callback: injectDependenciesAndConstants
+        };
+        if (this.options['skip-install']) {
+            this.log(logMsg);
+        } else {
+            this.installDependencies(installConfig);
+        }
+    }
+
+    end() {
+        this.log('End of angular-material-integration generator');
+    }
+};
